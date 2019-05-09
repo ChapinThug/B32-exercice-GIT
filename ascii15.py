fecba9f8376aad9c2a22334fc9a127208f41a7d0
Kevin Guerra <kevin.guerra22@hotmail.com>
2019-05-09 11:02:19 -0400

    JordanAsciiKG

--- ascii15.py
+++ ascii15.py
@@ -1,2 +1,9 @@
 def print_art():
 	print("Once upon a time, I was smiling at the sun and the sun wasn't smiling.")
+print("|__  o\ ")
+print("| W    \O")
+print("|       |\_  ")
+print("|      /-\   ")
+print("|    /     \   ")
+print("|             ")
+print("|               ")
